4645737545256ae46e2e985ea2d6ea2555990020
HelloWorldAn <1509876866@qq.com>
2018-04-06 20:51:34 +0800

    TestGit2 commit 01

---
+++ src/com/atguigu/git/GitTest2.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author AYL
+ * @create 2018年4月6日 下午8:49:31
+ */
+public class GitTest2 {
+	public static void main(String[] args) {
+		System.out.println("hello server 这是测试2");
+	}
+
+}
